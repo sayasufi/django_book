9fe1eeba23f0349b5ace9803f35dd6f14226c0ba
semyon <semyon.ryaboff@yandex.ru>
2024-02-26 18:41:04 +0300

    Recommendation of posts by email mail

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,7 +1,8 @@
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -9,27 +10,39 @@ class PostListView(ListView):
     """
     Альтернативное представление списка постов
     """
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
 
-def post_list(request):
-    post_list = Post.published.all()
-    # Постраничная разбивка с 3 постами на страницу
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get("page", 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # Если page_number не целое число, то
-        # выдать первую страницу
-        posts = paginator.page(1)
-    except EmptyPage:
-        # Если page_number находится вне диапазона, то
-        # выдать последнюю страницу
-        posts = paginator.page(paginator.num_pages)
-    return render(request, "blog/post/list.html", {"posts": posts})
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+
+
+def post_share(request, post_id):
+    # Извлечь пост по идентификатору id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == "POST":
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            send_mail(subject, message, "ryaboff.sem@gmail.com", [cd["to"]])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        "blog/post/share.html",
+        {"post": post, "form": form, "sent": sent},
+    )
 
 
 def post_detail(request, year, month, day, post):
--- blog/mysite/settings.py
+++ blog/mysite/settings.py
@@ -124,3 +124,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Конфигурация сервера электронной почты
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "ryaboff.sem@gmail.com"
+EMAIL_HOST_PASSWORD = "wodqenlrneezfylx "
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
