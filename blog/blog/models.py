8a70d6b8340c95c1be48fc4411a4a27f3ef1bcdd
semyon <semyon.ryaboff@yandex.ru>
2024-02-26 16:42:27 +0300

    Creation of model managers

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -20,6 +25,8 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    objects = models.Manager()  # менеджер, применяемый по умолчанию
+    published = PublishedManager()  # конкретно-прикладной менеджер
 
     class Meta:
         ordering = ['-publish']
