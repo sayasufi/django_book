0e7a5919add0135e76fd1167f38208de20b88fb8
semyon <semyon.ryaboff@yandex.ru>
2024-02-27 17:50:14 +0300

    Change password views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -10,5 +10,14 @@ urlpatterns = [
     # url-адреса входа и выхода
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
+    # url-адреса смены пароля
+    path('password-change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password-change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+
     path('', views.dashboard, name='dashboard'),
 ]
