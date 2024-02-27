ea4903ce6ef7eff783c2fcd72c134dae314743db
semyon <semyon.ryaboff@yandex.ru>
2024-02-27 14:39:03 +0300

    Creating news feeds for posts blog

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .feeds import LatestPostsFeed
 from .sitemaps import PostSitemap
 
 
@@ -18,4 +19,5 @@ urlpatterns = [
     ),
     path("<int:post_id>/share/", views.post_share, name="post_share"),
     path("<int:post_id>/comment/", views.post_comment, name="post_comment"),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
